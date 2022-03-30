d97da8bc2e66beb34a89685c6da14414f795386e
Mattis Jaksch <Mattis.Jaksch@dlr.de>
2022-03-30 14:55:38 +0200

    Added logging functions

--- src/Manager/FileManager.h
+++ src/Manager/FileManager.h
@@ -50,6 +50,7 @@ public:
     void append_value_to_file(Component* component, double value);
     void append_value_to_file(Component* component, const QString& value);
     void append_values_to_file(Component* component, const QVector<double>& values);
+    void append_values_to_file(Component* component, const QVector<QVector<double>>& values);
     void append_values_to_file(Component* component, const QStringList& values);
 
     void register_component(Component* component);
@@ -62,7 +63,7 @@ protected:
 
     bool file_exists(const QFile* ref_file);
 
-    QString vector_to_string(const QVector<double>& vector);
+    QString vector_to_string(const QVector<double>& vector, char seperator = ';');
 
     void update_root_directory();
 
@@ -94,15 +95,31 @@ inline void FileManager::append_value_to_file(Component* component, const QStrin
     get_file(component)->file_p->flush();
 }
 
-inline void FileManager::append_values_to_file(Component* component, const QVector<double> &values)
+inline void FileManager::append_values_to_file(Component* component, const QVector<double>& values)
 {
     long long  time = QDateTime::currentMSecsSinceEpoch() - get_file(component)->creation_time*1000;
 
     QVector<double> internal_values;
-//    internal_values.push_back(time);
     internal_values.append(values);
 
     QString res = QString::number(time) + ";" + vector_to_string(internal_values) + "\n";
+
+    get_file(component)->file_p->write(res.toUtf8());
+    get_file(component)->file_p->flush();
+}
+
+inline void FileManager::append_values_to_file(Component* component, const QVector<QVector<double>>& values)
+{
+    long long  time = QDateTime::currentMSecsSinceEpoch() - get_file(component)->creation_time*1000;
+
+    QStringList internal_values;
+
+    foreach(QVector<double> val_vec, values)
+    {
+        internal_values.append(vector_to_string(val_vec, ','));
+    }
+
+    QString res = QString::number(time) + ";" + internal_values.join(';') + "\n";
 
     get_file(component)->file_p->write(res.toUtf8());
     get_file(component)->file_p->flush();
@@ -129,14 +146,14 @@ inline void FileManager::set_file_header(Component* component, const QStringList
     get_file(component)->file_p->flush();
 }
 
-inline QString FileManager::vector_to_string(const QVector<double>& vector)
+inline QString FileManager::vector_to_string(const QVector<double>& vector, char seperator)
 {
     QString msg = "";
     QVectorIterator<double> it(vector);
 
     while(it.hasNext())
     {
-        msg += QString::number(it.next()) + (it.hasNext() ? ";" : "");
+        msg += QString::number(it.next()) + (it.hasNext() ? QString(seperator) : "");
     }
 
     return msg;
